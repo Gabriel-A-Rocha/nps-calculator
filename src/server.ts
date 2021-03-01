97878d702de38c40527264fd10016490f6c88218
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-02-28 20:51:51 -0500

    feat: add user creation route

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import "reflect-metadata";
 import "./database";
 import express from "express";
+import { router } from "./routes";
 
 const app = express();
 
-const port = 3333;
+app.use(express.json());
 
+const port = 3333;
 app.listen(port, () => console.log(`Server running at port ${port}`));
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello world!!!" });
-});
+app.use(router);
